b67cb2ce5cd10bd1508787a8700dade14a6a218d
Jimdan1 <dollarsplash@gmail.com>
2022-09-08 23:37:20 -0700

    task 4-8

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+
+/**
+
+ * main - Printing programming is like building
+
+ *
+
+ * Return: Always 0 (Successfully)
+
+ */
+
+int main(void)
+  
+{
+  
+  puts("\"Programming is like building a multilingual
+
+puzzle");
+  
+  return (0);
+  
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+
+/**
+
+ * main - Prints a text using printf function
+
+ *
+
+ * Return: Always 0 (Successfully)
+
+ */
+
+int main(void)
+  
+{
+  
+  printf("with proper grammar, but the outcome is a piece of
+
+art,\n");
+  
+  return (0);
+  
+}
