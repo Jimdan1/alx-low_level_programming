204c505135df0017f744660e6e6b032414040116
Jimdan1 <dollarsplash@gmail.com>
2022-09-09 11:09:05 -0700

    task 4-puts - 6-size.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,25 +1,12 @@
 #include <stdio.h>
-
-
-
 /**
-
  * main - Printing programming is like building
-
  *
-
  * Return: Always 0 (Successfully)
-
  */
-
 int main(void)
-  
 {
-  
   puts("\"Programming is like building a multilingual
-
 puzzle");
-  
   return (0);
-  
 }
