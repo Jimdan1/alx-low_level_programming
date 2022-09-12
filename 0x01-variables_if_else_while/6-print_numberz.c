68b676b0999b048aed1804352c127df2f7582904
Jimdan1 <dollarsplash@gmail.com>
2022-09-12 04:40:10 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,13 +3,15 @@
 * main - print all single digit numbers of base 10 starting from 0
 *
 * Description : using main funtion
-* A script to print all single digit numbers of base 10 starting form 0
+* This program is a script to print all single digit numbers of base 10
 * return: 0
 */
+
 int main(void)
+
 {
 int d;
-for (d = '0'; d <= '9'; d++)
+for (d = 0; d <= 9; d++)
 {
 putchar(d);
 }
